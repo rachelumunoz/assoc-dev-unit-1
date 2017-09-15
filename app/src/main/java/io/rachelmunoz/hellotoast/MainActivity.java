c683f3d9a892118180590f8ec1ec73df5d3e1bfc
rachelmunoz <rachelumunoz>
2017-09-15 13:09:26 -0700

    Adds onClick handlers, finishes 1.2A

--- app/src/main/java/io/rachelmunoz/hellotoast/MainActivity.java
+++ app/src/main/java/io/rachelmunoz/hellotoast/MainActivity.java
@@ -2,12 +2,29 @@ package io.rachelmunoz.hellotoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+	private int mCount = 0;
+	private TextView mShowCount;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+		mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+	public void showToast(View view) {
+		Toast.makeText(this, R.string.toast_message, Toast.LENGTH_SHORT).show();
+	}
+
+
+	public void countUp(View view) {
+		mCount++;
+		mShowCount.setText(String.valueOf(mCount));
 	}
 }
